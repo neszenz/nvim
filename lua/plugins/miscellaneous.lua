6e9dc9999efbe359b226d40ea5a6b5de697fb835
Tobias Newe <tobias.newe@t-online.de>
2023-09-09 22:14:18 +0200

    Add some more keybindings

--- lua/plugins/miscellaneous.lua
+++ lua/plugins/miscellaneous.lua
@@ -41,13 +41,13 @@ return {
 
                 -- don't override the built-in and fugitive keymaps
                 local gs = package.loaded.gitsigns
-                vim.keymap.set({'n', 'v'}, ']c', function()
-                    if vim.wo.diff then return ']c' end
+                vim.keymap.set({'n', 'v'}, ']h', function()
+                    if vim.wo.diff then return ']h' end
                     vim.schedule(function() gs.next_hunk() end)
                     return '<Ignore>'
                 end, {expr=true, buffer = bufnr, desc = "Jump to next hunk"})
-                vim.keymap.set({'n', 'v'}, '[c', function()
-                    if vim.wo.diff then return '[c' end
+                vim.keymap.set({'n', 'v'}, '[h', function()
+                    if vim.wo.diff then return '[h' end
                     vim.schedule(function() gs.prev_hunk() end)
                     return '<Ignore>'
                 end, {expr=true, buffer = bufnr, desc = "Jump to previous hunk"})
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,29 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.2",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader><leader>', builtin.find_files, {desc = 'Telescope find_files'})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Telescope buffers'})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = 'Telescope help_tags'})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {desc = 'Telescope git_files'})
-        vim.keymap.set('n', '<leader>ps', function() builtin.grep_string({ search = vim.fn.input("Filter: ") }); end, {desc = 'Telescope filtered grep_string'})
-        vim.keymap.set('n', '<leader>pg', builtin.live_grep, {desc = 'Telescope live_grep'})
-        vim.keymap.set('n', '<leader>pc', builtin.git_commits, {desc = 'Telescope git_commits'})
+
+        vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = 'Telescope oldfiles' })
+
+        vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope git_files' })
+        vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = 'Telescope git_commits' })
+
+        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Telescope find_files' })
+        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Telescope help_tags' })
+        vim.keymap.set('n', '<leader>sw', function() builtin.grep_string({ search = vim.fn.input('Filter: ') }); end, { desc = 'Telescope grep_string (filtered)' })
+        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope live_grep' })
+        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = 'Telescope diagnostics' })
+        vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = 'Telescope resume' })
+
+        vim.keymap.set('n', '<leader>/', function()
+            builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                winblend = 10,
+                previewer = false,
+            })
+        end, { desc = 'Telescope current_buffer_fuzzy_find' })
 
         -- Enable telescope fzf native if installed
         pcall(require('telescope').load_extension, 'fzf')
