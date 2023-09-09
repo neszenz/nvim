a7b45cff35144e4c49ac8ca6e8339745b74f7d6f
Tobias Newe <tobias.newe@t-online.de>
2023-09-09 20:39:42 +0200

    Unify misc packages and add some more

---
+++ lua/plugins/miscellaneous.lua
@@ -0,0 +1,57 @@
+return {
+    -- git integration
+    'tpope/vim-fugitive',
+
+    -- indentation auto-config
+    'tpope/vim-sleuth',
+
+     -- "gc" to comment visual regions/lines
+     { 'numToStr/Comment.nvim', opts = {} },
+
+    -- Add indentation guides even on blank lines
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        opts = {
+            char = '┊',
+            show_trailing_blankline_indent = false,
+        },
+    },
+
+    -- pending keybinding viewer
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {}
+    },
+
+    -- Adds git related signs to the gutter, as well as utilities for managing changes
+    {
+        'lewis6991/gitsigns.nvim',
+        opts = {
+            -- See `:help gitsigns.txt`
+            signs = {
+                add = { text = '+' },
+                change = { text = '~' },
+                delete = { text = '_' },
+                topdelete = { text = '‾' },
+                changedelete = { text = '~' },
+            },
+            on_attach = function(bufnr)
+                vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
+
+                -- don't override the built-in and fugitive keymaps
+                local gs = package.loaded.gitsigns
+                vim.keymap.set({'n', 'v'}, ']c', function()
+                    if vim.wo.diff then return ']c' end
+                    vim.schedule(function() gs.next_hunk() end)
+                    return '<Ignore>'
+                end, {expr=true, buffer = bufnr, desc = "Jump to next hunk"})
+                vim.keymap.set({'n', 'v'}, '[c', function()
+                    if vim.wo.diff then return '[c' end
+                    vim.schedule(function() gs.prev_hunk() end)
+                    return '<Ignore>'
+                end, {expr=true, buffer = bufnr, desc = "Jump to previous hunk"})
+            end,
+        },
+    },
+}
