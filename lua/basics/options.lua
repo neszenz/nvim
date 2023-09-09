e2ccf5fd1426c1350492d7aa07c04143441d3a79
Tobias Newe <tobias.newe@t-online.de>
2023-09-09 20:44:05 +0200

    Set basic options

--- lua/basics/options.lua
+++ lua/basics/options.lua
@@ -2,4 +2,26 @@ vim.opt.number = true
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+
 vim.opt.swapfile = false
+vim.opt.undofile = true
+
+vim.opt.signcolumn = 'yes'
+
+vim.opt.mouse = 'n'
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.breakindent = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.updatetime = 250
+
+vim.opt.timeout = true
+vim.opt.timeoutlen = 300
+
+vim.opt.completeopt = 'menuone,noselect'
+
+vim.opt.termguicolors = true
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,12 +4,15 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Telescope find_files'})
+        vim.keymap.set('n', '<leader><leader>', builtin.find_files, {desc = 'Telescope find_files'})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Telescope buffers'})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = 'Telescope help_tags'})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {desc = 'Telescope git_files'})
         vim.keymap.set('n', '<leader>ps', function() builtin.grep_string({ search = vim.fn.input("Filter: ") }); end, {desc = 'Telescope filtered grep_string'})
         vim.keymap.set('n', '<leader>pg', builtin.live_grep, {desc = 'Telescope live_grep'})
         vim.keymap.set('n', '<leader>pc', builtin.git_commits, {desc = 'Telescope git_commits'})
+
+        -- Enable telescope fzf native if installed
+        pcall(require('telescope').load_extension, 'fzf')
     end
 }
