d1e3454800534bf2d1fb15a140297fda2c30eb2b
Tobias Newe <tobias.newe@t-online.de>
2023-09-10 20:34:52 +0200

    Auto-Commands for testing

--- lua/basics/options.lua
+++ lua/basics/options.lua
@@ -33,3 +33,28 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     group = highlight_group,
     pattern = '*',
 })
+
+-- Auto-Commands for testing
+--vim.cmd[[autocmd BufRead * echo 'BufRead']]
+--vim.cmd[[autocmd BufWrite * echo 'BufWrite']]
+-- vim.cmd[[autocmd BufNewFile * echo 'BufNewFile']]
+-- vim.cmd[[autocmd BufEnter * echo 'BufEnter']]
+-- vim.cmd[[autocmd BufLeave * echo 'BufLeave']]
+-- vim.cmd[[autocmd BufWinEnter * echo 'BufWinEnter']]
+-- vim.cmd[[autocmd BufWinLeave * echo 'BufWinLeave']]
+-- vim.cmd[[autocmd FileReadPre * echo 'FileReadPre']]
+-- vim.cmd[[autocmd FileReadPost * echo 'FileReadPost']]
+-- vim.cmd[[autocmd FileWritePre * echo 'FileWritePre']]
+-- vim.cmd[[autocmd FileWritePost * echo 'FileWritePost']]
+-- vim.cmd[[autocmd CursorMoved * echo 'CursorMoved']]
+-- vim.cmd[[autocmd CursorMovedI * echo 'CursorMovedI']]
+-- vim.cmd[[autocmd VimEnter * echo 'VimEnter']]
+-- vim.cmd[[autocmd VimLeavePre * echo 'VimLeavePre']]
+-- vim.cmd[[autocmd FocusGained * echo 'FocusGained']]
+-- vim.cmd[[autocmd FocusLost * echo 'FocusLost']]
+-- vim.cmd[[autocmd WinEnter * echo 'WinEnter']]
+-- vim.cmd[[autocmd WinLeave * echo 'WinLeave']]
+-- vim.cmd[[autocmd CmdlineEnter * echo 'CmdlineEnter']]
+-- vim.cmd[[autocmd CmdlineLeave * echo 'CmdlineLeave']]
+-- vim.cmd[[autocmd InsertEnter * echo 'InsertEnter']]
+-- vim.cmd[[autocmd InsertLeave * echo 'InsertLeave']]
