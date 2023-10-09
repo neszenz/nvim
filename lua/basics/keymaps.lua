7df64e5c59047507ca5705e2fe966a22c4979429
Tobias Newe <newe@knick.de>
2023-10-09 17:53:10 +0200

    Add window switching shortcuts

--- lua/basics/keymaps.lua
+++ lua/basics/keymaps.lua
@@ -11,6 +11,10 @@ vim.keymap.set('n', '<leader>gD', ':Git diff --staged<CR>_', {})
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', '<C-h>', '<C-w>h', {})
+vim.keymap.set('n', '<C-j>', '<C-w>j', {})
+vim.keymap.set('n', '<C-k>', '<C-w>k', {})
+vim.keymap.set('n', '<C-l>', '<C-w>l', {})
 
 -- Keeping it in center
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
