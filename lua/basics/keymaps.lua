4ba60608fc7a8ed6c5c79754f8fd88336d28bbc5
Tobias Newe <newe@knick.de>
2023-10-12 17:19:40 +0200

    Add include macro helper

--- lua/basics/keymaps.lua
+++ lua/basics/keymaps.lua
@@ -28,6 +28,9 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+vim.keymap.set('i', '#<', '#include <><esc>i', {})
+vim.keymap.set('i', '#"', '#include ""<esc>i', {})
+
 vim.keymap.set({'n','v','i'}, '<M-C-/>', '0', {})
 vim.keymap.set({'n','v','i'}, '<M-C-m>', '1', {})
 vim.keymap.set({'n','v','i'}, '<M-C-,>', '2', {})
