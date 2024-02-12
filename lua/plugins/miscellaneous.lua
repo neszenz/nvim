44a00a540eed6a53e1598d1d26fbec9af5772643
Tobias Newe <tobias.newe@t-online.de>
2024-02-12 15:18:50 +0100

    Add sindrets/diffview.nvim

--- lua/plugins/miscellaneous.lua
+++ lua/plugins/miscellaneous.lua
@@ -56,6 +56,10 @@ return {
         end
     },
 
+    {
+        'sindrets/diffview.nvim'
+    },
+
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     {
         'lewis6991/gitsigns.nvim',
