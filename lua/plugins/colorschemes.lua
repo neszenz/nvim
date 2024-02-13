13b3ac6c1748a3973a079ce26aa0a0c833ac5979
Tobias Newe <tobias.newe@t-online.de>
2024-02-13 16:01:11 +0100

    Update colorscheme and add lualine

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,10 +3,50 @@ return {
         'navarasu/onedark.nvim',
         config = function()
             require('onedark').setup {
-                style = 'dark',
+                style = 'cool',
                 transparent = true
             }
             require('onedark').load()
         end
-    }
+    },
+    {
+        'neanias/everforest-nvim',
+        config = function ()
+            require('everforest').setup {
+                background = "hard",
+                transparent_background_level = 1,
+                ui_contrast = "high",
+            }
+            -- require('everforest').load()
+        end
+    },
+    {
+        'tiagovla/tokyodark.nvim',
+        config = function ()
+            require("tokyodark").setup {
+                transparent_background = true,
+                gamma = 1.2,
+            }
+            -- vim.cmd [[colorscheme tokyodark]]
+        end
+    },
+    {
+        'franbach/miramare',
+        config = function ()
+            vim.cmd [[let g:miramare_transparent_background = 1]]
+        end
+    },
+    {
+        'rebelot/kanagawa.nvim',
+        config = function ()
+            require("kanagawa").setup {
+                transparent = true,
+            }
+            -- vim.cmd[[colorscheme kanagawa]]
+        end
+    },
+
+    'Mofiqul/vscode.nvim',
+
+    'gibfahn/gib-noir.nvim',
 }
