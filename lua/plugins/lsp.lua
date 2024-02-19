9aab87849ec6800bb6a0098fd4e2b7074b52f1fc
Tobias Newe <tobias.newe@t-online.de>
2024-02-19 19:51:29 +0100

    Update cmp selection keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -137,28 +137,25 @@ return {
                 end,
             },
             mapping = cmp.mapping.preset.insert {
+                ['<C-Space>'] = cmp.mapping.complete(),
                 ['<C-n>'] = cmp.mapping.select_next_item(),
                 ['<C-p>'] = cmp.mapping.select_prev_item(),
-                ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete {},
-                ['<CR>'] = cmp.mapping.confirm {
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<C-y>'] = cmp.mapping.confirm {
                     behavior = cmp.ConfirmBehavior.Replace,
                     select = true,
                 },
                 ['<Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.expand_or_locally_jumpable() then
+                    if luasnip.expand_or_locally_jumpable() then
                         luasnip.expand_or_jump()
                     else
                         fallback()
                     end
                 end, { 'i', 's' }),
                 ['<S-Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.locally_jumpable(-1) then
+                    if luasnip.locally_jumpable(-1) then
                         luasnip.jump(-1)
                     else
                         fallback()
