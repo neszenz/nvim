94dccadb22cc645ddf7a02bc9651b9f3e83bbcd9
Tobias Newe <tobias.newe@t-online.de>
2024-02-26 17:18:01 +0100

    Remove aerial step keybinding

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -1,16 +1,12 @@
 return {
     'stevearc/aerial.nvim',
-    opts = {},
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
     },
     opts = {
         -- optionally use on_attach to set keymaps when aerial has attached to a buffer
         on_attach = function(bufnr)
-            -- Jump forwards/backwards with '{' and '}'
-            vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
-            vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
-            vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle left<CR>', {})
+            vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle left<CR>', { buffer = bufnr})
         end,
         keymaps = {
             ["?"] = "actions.show_help",
