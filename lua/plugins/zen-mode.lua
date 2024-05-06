28de3cd96465188995e2b7e1b130ce9614088ea4
Tobias Newe <tobias.newe@t-online.de>
2024-05-06 13:26:58 +0200

    Improve zen mode behaviour

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -28,14 +28,14 @@ return {
                 options = {
                     enabled = true,
                     ruler = false, -- disables the ruler text in the cmd line area
-                    showcmd = false, -- disables the command in the last line of the screen
+                    showcmd = true, -- disables the command in the last line of the screen
                     -- you may turn on/off statusline in zen mode by setting 'laststatus' 
                     -- statusline will be shown only if 'laststatus' == 3
-                    laststatus = 0, -- turn off the statusline in zen mode
+                    laststatus = 3, -- turn off the statusline in zen mode
                 },
                 twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
                 gitsigns = { enabled = true }, -- disables git signs
-                tmux = { enabled = false }, -- disables the tmux statusline
+                tmux = { enabled = true }, -- disables the tmux statusline
                 -- this will change the font size on kitty when in zen mode
                 -- to make this work, you need to set the following kitty options:
                 -- - allow_remote_control socket-only
