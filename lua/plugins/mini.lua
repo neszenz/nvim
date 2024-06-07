09ad1dad53e2ebdc4c61f23adc0c448f24ec87d1
Tobias Newe <tobias.newe@t-online.de>
2024-06-07 15:19:45 +0200

    Add mini.ai

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        'echasnovski/mini.ai',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects'
+        },
+        verion = false,
+        config = {},
+    },
+}
